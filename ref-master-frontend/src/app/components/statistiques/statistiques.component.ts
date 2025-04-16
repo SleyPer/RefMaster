87fad753aff49b45b31a7552a00bdbfb08fd3450
SleyPer <prevostt76@gmail.com>
2025-04-16 02:04:12 +0200

    Modification visuelle de la page des statistiques

--- ref-master-frontend/src/app/components/statistiques/statistiques.component.ts
+++ ref-master-frontend/src/app/components/statistiques/statistiques.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { StatistiquesService } from 'src/app/services/statistiques.service';
 import { StatistiquesGlobalesDto, StatistiquesParAnneeDto } from 'src/app/models/statistiques.model';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-statistiques',
   templateUrl: './statistiques.component.html',
-  styleUrls: ['./statistiques.component.scss']
+  styleUrls: ['./statistiques.component.scss'],
+  animations: [
+    trigger('slideToggle', [
+      transition(':enter', [
+        style({ height: 0, opacity: 0 }),
+        animate('300ms ease-out', style({ height: '*', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ height: '*', opacity: 1 }),
+        animate('300ms ease-in', style({ height: 0, opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class StatistiquesComponent implements OnInit {
   statsGlobales!: StatistiquesGlobalesDto;
@@ -16,6 +29,8 @@ export class StatistiquesComponent implements OnInit {
     'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'
   ];
 
+  anneesOuvertes: boolean[] = [];
+
   constructor(private statistiquesService: StatistiquesService) { }
 
   ngOnInit(): void {
@@ -23,5 +38,11 @@ export class StatistiquesComponent implements OnInit {
       this.statsGlobales = stats;
       this.statsParAnnee = stats.parAnnee.sort((a, b) => a.annee - b.annee);
     });
+
+    this.anneesOuvertes = this.statsParAnnee.map(() => true);
+  }
+
+  toggleAnnee(index: number): void {
+    this.anneesOuvertes[index] = !this.anneesOuvertes[index];
   }
 }
